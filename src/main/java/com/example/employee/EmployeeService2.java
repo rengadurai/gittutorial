ab88273330d4ff415cf0bdbda3624ccfd8db49d8
rengadurai <122352408+rengadurai@users.noreply.github.com>
2023-08-29 01:24:56 +0530

    Update EmployeeService2.java

--- src/main/java/com/example/employee/EmployeeService2.java
+++ src/main/java/com/example/employee/EmployeeService2.java
@@ -8,7 +8,6 @@ public class EmployeeService2 {
 	@Autowired
 	EmployeeDao empDao;
 	System.out.println("new emergency branch");
-	System.out.println("editing code in git");
 	System.out.println("checking git pull");
 	System.out.println("new branch creating in github");
 	
